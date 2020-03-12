4706d216566a4dd6fcf722f9bb3c259fb5d89192
Хисматулина Карина <hkarinaf@gmail.com>
2020-03-12 23:20:31 +0300

    Add project files.

---
+++ Hash_Table/HashTable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash_Table
+{
+    public class HashTable<T>
+    {
+        private T[] items;
+        public HashTable(int size)
+        {
+            if (size > 0)
+            {
+                items = new T[size];
+            }
+        }
+
+        public void Add(T item)
+        {
+            int key = GetHash(item);
+            items[key] = item;
+        }
+
+        public bool Search(T item)
+        {
+            int key = GetHash(item);
+            bool res = items[key].Equals(item);
+            if (res == true) Console.WriteLine("this item is included to your hashtable");
+            else Console.WriteLine("this item wasn't find");
+            return res;
+
+        }
+
+        private int GetHash(T item)
+        {
+            return item.ToString().Length % items.Length; //bad hash function
+        }
+    }
+}
---
+++ Hash_Table/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashTable<int> ht = new HashTable<int>(5);
+            ht.Add(1);
+            ht.Add(9492);
+            ht.Add(777);
+
+            ht.Search(61);
+            ht.Search(184);
+            ht.Search(777);
+
+            Console.ReadLine();
+        }
+    }
+}
